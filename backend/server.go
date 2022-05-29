c20aa6524c7643505e16a81697f5564edff21cd8
Devon Berry <devberry@pdx.edu>
2022-05-28 20:56:16 -0700

    Updated to router

--- backend/server.go
+++ backend/server.go
@@ -181,16 +181,18 @@ func main() {
 	//Parse any matching *.html files
 	tpl, _ = tpl.ParseGlob("templates/*.html")
 
+	router := http.NewServeMux()
+
 	//handling HTML CSS from /static/ directory
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	//PATH handling
-	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/about", AboutHandler)
-	http.HandleFunc("/contact", ContactHandler)
-	http.HandleFunc("/login", LoginHandler)
-
+	router.Handle("/", http.NotFoundHandler())
+	router.Handle("/about", http.HandlerFunc(AboutHandler))
+	router.Handle("/contact", http.HandlerFunc(ContactHandler))
+	router.Handle("/login", http.HandlerFunc(LoginHandler))
+	
 	//Start Server, set PORT to 8080
 	//127.0.0.1:8080 for DEV, change back to :8080 for prod
 	fmt.Println("Server Listening on PORT 8080...")
